acd217561d33be71d4e9c18bc8e3397404accc9a
Varchenko Michael <mishanjav2001@gmail.com>
2021-02-03 18:42:24 +0200

    fix2.0

--- src/test/java/com/varchenko/FibonacciSequenceTest.java
+++ src/test/java/com/varchenko/FibonacciSequenceTest.java
@@ -5,17 +5,25 @@ import org.junit.jupiter.api.Test;
 import static com.varchenko.FibonacciSequence.fib;
 
 public class FibonacciSequenceTest {
+    FibonacciSequence fibonacciSequence = new FibonacciSequence() ;
+        @Test
+        void Fibonacci_numbersFrom0to10() {
+            for (int index = 0; index < 10; index++) {
 
-    @Test
-        void Fibonacci_number() {
-            for (int index=0; index<10; index++){
+                int expectedResult = fib(index);
 
-            int expectedResult = fib(index);
-
-            int result = fib(index);
-            assertEquals(expectedResult, result);
-            System.out.println(fib(index));
+                int result = fib(index);
+                assertEquals(expectedResult, result);
+                System.out.println(fib(index));
+            }
         }
-}
 
-}
+        @Test
+        void Fibonacci_SpecificNumber() {
+            int expectedResult = fib(10);
+            int result = 55;
+
+            assertEquals(expectedResult, result);
+            System.out.println(fib(10));
+        }
+    }
