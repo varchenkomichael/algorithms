e939f4459199970c2c47ffa43433cbd3a8fa596f
Varchenko Michael <mishanjav2001@gmail.com>
2021-04-20 15:53:40 +0300

    update N18_TrimToSize

--- src/main/java/com/varchenko/collections/basic/arrayList/N18_TrimToSizeCapacity.java
+++ src/main/java/com/varchenko/collections/basic/arrayList/N18_TrimToSizeCapacity.java
@@ -4,14 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class N18_TrimToSizeCapacity<T> {
-    private final List<T> list;
-
-    public N18_TrimToSizeCapacity(List<T> list) {
-        this.list = list;
-    }
 
     public List<T> trimToSizeArrayList(List<T> listOfLetter) {
         new ArrayList<>(listOfLetter).trimToSize();
-        return list;
+        return listOfLetter;
     }
 }
--- src/test/java/com/varchenko/collections/basic/arrayList/N18_TrimToSizeCapacityTest.java
+++ src/test/java/com/varchenko/collections/basic/arrayList/N18_TrimToSizeCapacityTest.java
@@ -2,6 +2,7 @@ package com.varchenko.collections.basic.arrayList;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,11 +11,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class N18_TrimToSizeCapacityTest {
     @Test
-    void trimToSize() {
-        List<String> helpList = new ArrayList<>(Arrays.asList("a", "b", "c"));
-        N18_TrimToSizeCapacity<String> n18_trimToSizeCapacity = new N18_TrimToSizeCapacity<>(helpList);
-        List<String> result = n18_trimToSizeCapacity.trimToSizeArrayList(helpList);
-        assertEquals(helpList, result);
+    void trimToSize() throws Exception {
+        List<String> helpList = new ArrayList<>(Arrays.asList("a", "b", "c", "d"));
+        N18_TrimToSizeCapacity<String> n18_trimToSizeCapacity = new N18_TrimToSizeCapacity<>();
+        List<String> sizeResult = n18_trimToSizeCapacity.trimToSizeArrayList(helpList);
+        int result =  getCapacity(helpList);
+        int expectedResult = sizeResult.size();
+        assertEquals(expectedResult, result);
     }
-
+    static int getCapacity(List<String> l) throws Exception {
+        Field dataField = ArrayList.class.getDeclaredField("elementData");
+        dataField.setAccessible(true);
+        return ((Object[]) dataField.get(l)).length;
+    }
 }
